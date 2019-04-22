b9e3ce3f5e3db6770ee90e6003b34bb6275d16fa
Mikayda Mills <mills.ma12@gmail.com>
2019-04-22 19:24:48 -0400

    added style to about me page

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -10,23 +10,37 @@ const AboutMe = ({ profileImg }) => {
         <h1 id="about-me" className="about-me-title">
           about me
         </h1>
-        <div className="about-me-content">
-          <p>this is the about me content. pretend there is stuff here.</p>
+        <div className="about-me-content-container">
+          <div className="about-me-content general">
+            <h2>Hi, I'm Mikayda Mills.</h2>
+            <p>
+              i live in nyc. my husband and i are both devs. so is our dog.
+              diggin' my groovy vibes? 'life is nothing w/o a theme' - emily
+              dickinson #girlswhocode #lawyerswhocode
+            </p>
+          </div>
+          <div className="about-me-content goals">
+            <h2>My Goalz</h2>
+            <p>
+              Be of use, make an impact, create world-changing tech, complete
+              the mission, dance in all of the meadows, watch a baseball game in
+              every major league stadium.
+            </p>
+          </div>
+          <div className="about-me-content topics">
+            <h2>My Topics</h2>
+            <p>
+              the Beatles, my dog Brady, the color green, geaux LSU Tigers,
+              baseball, John Irving novels, triathalons, gemstones, video games,
+              tarot card readings, yoga, crochet hooks, art galleries
+            </p>
+          </div>
         </div>
-        <div className="about-me-picture" />
-        <div className="about-me-content">
-          <h2>My Goals</h2>
-          <p>goalz</p>
+        <div className="about-me-picture-container">
+          <div className="about-me-picture" />
         </div>
-        <div className="about-me-content">
-          <h2>My Topics</h2>
-          <p>
-            my dog Brady, the Beatles, the color green, LSU Tigers, baseball,
-            John Irving novels, triathalons, gemstones, video games, tarot card
-            readings, yoga, crochet
-          </p>
-        </div>
-        <div className="about-me-content">
+
+        <div className="about-me-content resume">
           <h2>Link To Resume Here:</h2>
         </div>
       </div>
