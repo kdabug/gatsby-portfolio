4de9283b5fc26a120f27b4ddd230c2b4d4f9fd43
Mikayda Mills <mills.ma12@gmail.com>
2019-04-22 22:53:50 -0400

    cleaned up style for project cards

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -39,52 +39,52 @@ class Gif extends Component {
 
 export { Gif }
 
-const CardButton = styled.div`
-  width: 50%;
-  height: 35px;
-  border: 3px solid gray;
-  padding: 17px 30px 17px 30px;
-  position: relative;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-
-  &:hover {
-    color: red;
-  }
-`
-
 export const GifContainer = styled.div`
   margin-top: 10vh;
-  height: 80vh;
-  width: 90vw;
+  margin: 0 auto;
+  width: 75vw;
   display: flex;
   flex-wrap: wrap;
-  border: 1px solid red;
   justify-content: space-around;
 `
 
 export const GifWrap = styled.div`
   max-height: 357px;
-  border: 1px solid green;
-  width: 30%;
+  width: 35%;
   cursor: pointer;
-  margin: 0;
+  margin: 30px;
   padding: 0;
   display: flex;
   flex-direction: column;
+
+  &:hover {
+    transform: scale(1.05);
+    transition: all 1s;
+  }
   div {
     display: flex;
   }
   img {
-    /* border-radius: 1em; */
     padding: 0;
     margin: 0;
     height: 315px;
     width: 100%;
-    &:hover {
-      transform: scale(1.05);
-      transition: all 1s;
-    }
+  }
+`
+
+const CardButton = styled.div`
+  width: 50%;
+  height: 35px;
+  display: flex;
+  background: lightgray;
+  align-items: center;
+  border: 2px solid gray;
+  padding: 17px 30px 17px 30px;
+  justify-content: space-around;
+
+  &:hover {
+    color: white;
+    background: gray;
+    border: 2px solid lightgray;
   }
 `
